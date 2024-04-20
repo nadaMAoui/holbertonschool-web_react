726f1f12afe862dc84919a9cd8745339d3663c44
nadaMAoui <maaouinada747@gmail.com>
2024-04-20 07:33:31 +0200

    Renamed directory

---
+++ TypeScript/task_2/js/main.ts
@@ -0,0 +1,77 @@
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return 'Working from home';
+  }
+  getCoffeeBreak(): string {
+    return 'Getting a coffee break';
+  }
+  workDirectorTasks(): string {
+    return 'Getting to director tasks';
+  }
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return 'Cannot work from home';
+  }
+  getCoffeeBreak(): string {
+    return 'Cannot have a break';
+  }
+  workTeacherTasks(): string {
+    return 'Getting to work';
+  }
+}
+
+interface createEmployeeFunction {
+    (salary: string | number): Director | Teacher
+}
+
+
+const createEmployee: createEmployeeFunction = function (salary: number | string) {
+
+    if (typeof salary === 'string') {
+      salary = parseInt(salary)
+  }
+    else if (salary < 500) {
+        return new Teacher();
+    }
+    return new Director();
+}
+
+function isDirector(employee: Director | Teacher): employee is Director {
+    if (employee instanceof Director) {
+        return true;
+    }
+    return false;
+}
+
+function executeWork(employee: Director | Teacher): string {
+    if (isDirector(employee)) {
+        return employee.workDirectorTasks()
+    }
+    return employee.workTeacherTasks()
+}
+
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  }
+  return 'Teaching History';
+}
+
+console.log(teachClass('Math'));
+console.log(teachClass('History'));
